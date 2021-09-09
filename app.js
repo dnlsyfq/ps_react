ff313e4e6a8bb24561f3e31c45e6e9769b9070bf
Danial Syafiq <dan90@live.com.my>
2021-09-09 20:23:49 +0800

    update

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-// create react element
+const title = 'My First React Element';
+const desc = 'I just learned how to create a React node and render it into the DOM.';
+const myTitleID = 'main-title';
+const name = 'Dan';
 
-const title = React.element(
-    'h1',
-    {
-        id:'main-title',
-        title:'This is a title.',
+const header = (
+    <header>
+        <h1 id={myTitleID}>{name}'s First React Element</h1>
+        <p>{desc}</p>
+    </header>
+);
 
-    },
-    'My First React Element!'
+ReactDOM.render(
+    header,
+    document.getElementById('root')
 )
 
